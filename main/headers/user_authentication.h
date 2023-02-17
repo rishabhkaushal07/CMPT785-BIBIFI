bf8076bd77d8e7faf7db2dea2edab2f2ae4571d3
Ping Lu <97644115+awful996@users.noreply.github.com>
2023-02-16 21:14:02 -0800

    Update user_authentication.h

--- main/headers/user_authentication.h
+++ main/headers/user_authentication.h
@@ -5,11 +5,10 @@
 
 #include "encryption.h"
 #include <string>
-#include <openssl/sha.h>
 #include <random>
 
 using namespace std;
-void create_admin_keyfile(std::string keyfile_name) {
+/* void create_admin_keyfile(std::string keyfile_name) {
   std::fstream keyfile(keyfile_name, std::ios::out | std::ios::binary);
   if (!keyfile.is_open()) {
     std::cerr << "Error opening keyfile." << std::endl;
@@ -30,34 +29,11 @@ void create_admin_keyfile(std::string keyfile_name) {
   keyfile.close();
 }
 
-/* In main maybe add:
+In main maybe add:
   std::string keyfileName = "admin_keyfile";
   create_admin_keyfile(keyfileName);
   return 0;
 */
-
-
-bool is_valid_keyfile(const string &keyfile_name);
-std::ifstream keyfile(keyfile_name);
-if (!keyfile) {
-  std::cerr << "Error opening keyfile: " << keyfile_name << std::endl;
-  return false;
-}
-std::string keyfile_contents((std::istreambuf_iterator<char>(keyfile)),
-                             std::istreambuf_iterator<char>());
-unsigned char hash[SHA256_DIGEST_LENGTH];
-SHA256_CTX sha256;
-SHA256_Init(&sha256);
-SHA256_Update(&sha256, keyfile_contents.c_str(), keyfile_contents.length());
-SHA256_Final(hash, &sha256);
-std::string computed_hash = std::string((char *)hash, SHA256_DIGEST_LENGTH);
-// Compare computed_hash with expected_hash
-if (computed_hash == expected_hash) {
-  return true;
-} else {
-  return false;
-}
-# Note: This is just a sample code for validation and does not include the actual expected hash value, which should be stored securely in a separate location.
 
 string get_type_of_user(const std::string &keyfile_name) {
 
