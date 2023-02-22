379f1be0068e15f884779e32865b3a90de9f1452
Ping Lu <97644115+awful996@users.noreply.github.com>
2023-02-21 18:47:06 -0800

    Add add_user function
    Currently setting up the new environment and replacing deprecated functions in Openssl

--- main/headers/user_authentication.h
+++ main/headers/user_authentication.h
@@ -6,6 +6,69 @@
 #include "encryption.h"
 #include <string>
 #include <random>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <filesystem>
+#include <openssl/evp.h>
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+#include <openssl/err.h>
+
+using namespace std;
+void add_user(const string& username, const filesystem::path& public_key_dir, const filesystem::path& private_key_dir, bool admin = false) {
+    // Generate RSA key pair
+    RSA* rsa = RSA_new();
+    BIGNUM* bn = BN_new();
+    BN_set_word(bn, RSA_F4);
+    RSA_generate_key_ex(rsa, 2048, bn, NULL);
+
+    // Create EVP_PKEY object
+    EVP_PKEY* pkey = EVP_PKEY_new();
+    if (!pkey) {
+        cerr << "Failed to create EVP_PKEY object" << endl;
+        RSA_free(rsa);
+        BN_free(bn);
+        return;
+    }
+    EVP_PKEY_set1_RSA(pkey, rsa);
+
+    // Get public key
+    unsigned char* pub_key_der;
+    int pub_key_der_len = i2d_PublicKey(pkey, &pub_key_der);
+
+    // Write public key to file
+    string pub_key_filename = username + ".pub";
+    if (admin) {
+        pub_key_filename = "admin.pub";
+    }
+    filesystem::path pub_key_file = public_key_dir / pub_key_filename;
+    ofstream pub_key_stream(pub_key_file, ios::binary);
+    pub_key_stream.write((char*)pub_key_der, pub_key_der_len);
+    pub_key_stream.close();
+
+    // Get private key
+    unsigned char* pri_key_der;
+    int pri_key_der_len = i2d_PrivateKey(pkey, &pri_key_der);
+
+    // Write private key to file
+    string pri_key_filename = username + ".pri";
+    if (admin) {
+        pri_key_filename = "admin.pri";
+    }
+    filesystem::path pri_key_file = private_key_dir / pri_key_filename;
+    ofstream pri_key_stream(pri_key_file, ios::binary);
+    pri_key_stream.write((char*)pri_key_der, pri_key_der_len);
+    pri_key_stream.close();
+
+    // Clean up
+    RSA_free(rsa);
+    BN_free(bn);
+    EVP_PKEY_free(pkey);
+
+    cout << "User " << username << " added successfully." << endl;
+}
+
 
 using namespace std;
 /* void create_admin_keyfile(std::string keyfile_name) {
