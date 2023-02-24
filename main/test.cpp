14c303ebd6a9574f87a9d0ff74d1e882b527c4bd
Anupama Ravichandran <94922647+Anupama3799@users.noreply.github.com>
2023-02-24 13:44:54 -0500

    Testing  writing data into JSON

--- main/test.cpp
+++ main/test.cpp
@@ -3,16 +3,12 @@
 #include "headers/json.hpp"
 using namespace std;
 using json = nlohmann::json;
-int main()
-{
-std::ifstream f("metadata.json");
-json data = json::parse(f);
-// Access the values existing in JSON data
-string name = data.value("test", "not found");
-// Access a value that does not exist in the JSON data
-string check = data.value("test2", "not found");
-// Print the values
-cout << "Test: " << name << endl;
-cout << "Check: " << check << endl;
-return 0;
+
+int main() {
+    json jsonfile;
+
+    jsonfile["check"] = "123";
+
+    std::ofstream file("metadata.json");
+    file << jsonfile;
 }
