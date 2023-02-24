37961059e811e36d38520dd5c87a9adb92ef6c94
Anupama Ravichandran <94922647+Anupama3799@users.noreply.github.com>
2023-02-24 17:18:34 -0500

    TEST

--- main/encryption.cpp
+++ main/encryption.cpp
@@ -62,7 +62,7 @@ string filename_encryption(string filename){
     srand(time(NULL));
     //Generating the random string for filename
     string randomized_filename = Randomizer(10);
-
+    cout << randomized_filename << "\n";
     //Reading the metadata JSON for inserting the randomizer-filename mapping
     std::ifstream metadata_file("metadata.json");
     json metadata_json = json::parse(metadata_file);
