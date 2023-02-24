f38137b46b8e99987154b95659acca221f10d573
Anupama Ravichandran <94922647+Anupama3799@users.noreply.github.com>
2023-02-24 14:13:37 -0500

    Testing  writing data into JSON

--- main/test.cpp
+++ main/test.cpp
@@ -7,7 +7,7 @@ using json = nlohmann::json;
 int main() {
     std::ifstream f("metadata.json");
     json jsonfile = json::parse(f);
-    json insertVal = R"( {"check","123"} )"_json;
+    json insertVal = R"( {"check":"123"} )"_json;
     cout << jsonfile;
     jsonfile.update(insertVal.begin(), insertVal.end(), true);
     std::ofstream file("metadata.json");
