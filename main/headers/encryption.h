60d42dd018c2023e2e559cebeadd1027b8069622
Manvi Batra <manvibatra28@gmail.com>
2023-02-24 00:36:25 -0400

    updating header

--- main/headers/encryption.h
+++ main/headers/encryption.h
@@ -1,6 +1,11 @@
 #ifndef CMPT785_BIBIFI_ENCRYPTION_H
 #define CMPT785_BIBIFI_ENCRYPTION_H
 
+const int BLOCK_SIZE = 16; //bytes
+const int KEY_SIZE = 32; //bytes
+const int TAG_SIZE = 16; //bytes
+const int IV_SIZE = 16; //bytes
+
 void handleErrors(void);
 void encrypt_file(string filePath, string content, unsigned char *key);
 string decrypt_file(string filePath, unsigned char *key);
