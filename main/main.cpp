273695f8fa43207a51883d485096b7315ddd2af3
Manvi Batra <manvibatra28@gmail.com>
2023-02-24 12:26:13 -0400

    get user enc key from metadata and pass to user_features add metadata directory on first run accomodate return type change for get_type_of_user

--- main/main.cpp
+++ main/main.cpp
@@ -22,43 +22,48 @@ int main(int argc, char *argv[]) {
     string filesystem_path = fs::current_path();
 
     // TODO: check for user authentication
-    User_type user_type = get_type_of_user(keyfile_name);
-    if (user_type == admin || user_type == user) {
+    string user_name = get_type_of_user(keyfile_name);
+    User_type user_type;
+    if(user_name == "admin")
+      user_type = admin;
+    else
+      user_type = user;
 
-      // Create a filesystem, public_keys and private_keys directory, if none exists.
-      struct stat sb;
-      mode_t mode = 0666;
-      mode_t old_umask = umask(0); // to ensure the following modes get set
-      if (stat("filesystem", &sb) != 0) {
-        if (mkdir("filesystem", mode) != 0) {
-          std::cerr << "Error creating filesystem." << std::endl;
-          return 1;
-        }
+    // Create a filesystem, public_keys and private_keys directory, if none exists.
+    struct stat sb;
+    mode_t mode = 0666;
+    mode_t old_umask = umask(0); // to ensure the following modes get set
+    if (stat("filesystem", &sb) != 0) {
+      if (mkdir("filesystem", mode) != 0) {
+        std::cerr << "Error creating filesystem." << std::endl;
+        return 1;
       }
-      if (stat("public_keys", &sb) != 0) {
-        if (mkdir("public_keys", mode) != 0) {
-          std::cerr << "Error creating public_keys." << std::endl;
-          return 1;
-        }
-      }
-      if (stat("private_keys", &sb) != 0) {
-        if (mkdir("private_keys", mode) != 0) {
-          std::cerr << "Error creating private_keys." << std::endl;
-          return 1;
-        }
-      }
-      umask(old_umask); // Restore the original umask value
-
-      // user authenticated, allow "available commands" to be run
-      user_features(user_type, filesystem_path);
-    } else {
-      // Since the user wasn't authenticated, the login was failed and the
-      // program was exited.
-      cout << "Invalid keyfile" << endl;
-      // before exiting encrypt the filesystem again
-      encrypt_filesystem();
-      exit(EXIT_FAILURE);
     }
+    if (stat("public_keys", &sb) != 0) {
+      if (mkdir("public_keys", mode) != 0) {
+        std::cerr << "Error creating public_keys." << std::endl;
+        return 1;
+      }
+    }
+    if (stat("private_keys", &sb) != 0) {
+      if (mkdir("private_keys", mode) != 0) {
+        std::cerr << "Error creating private_keys." << std::endl;
+        return 1;
+      }
+    }
+    if (stat("metadata", &sb) != 0) {
+      if (mkdir("metadata", mode) != 0) {
+        std::cerr << "Error creating metadata directory." << std::endl;
+        return 1;
+      }
+    }
+    umask(old_umask); // Restore the original umask value
+
+    // user authenticated, allow "available commands" to be run
+
+    // read user's enc key from metadata file
+    uint8_t* key = read_enc_key_from_metadata(user_name);
+    user_features(user_name, user_type, key, filesystem_path);
 
     // before exiting encrypt the filesystem again
     encrypt_filesystem();
