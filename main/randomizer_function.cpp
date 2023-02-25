c962d0bc33bd69297c578dec899dc00f1808a0c2
Anupama Ravichandran <94922647+Anupama3799@users.noreply.github.com>
2023-02-25 15:31:52 -0800

    issue fix

--- main/randomizer_function.cpp
+++ main/randomizer_function.cpp
@@ -142,7 +142,8 @@ int main()
     encrypt_filename("dir1");
     encrypt_filename("dir2");
     string filepath = "dir1/dir2/testfile.txt";
-    cout << "Randomized path: " << get_randomized_file_path(filepath) << std::endl;
-    cout << "PT_path: " << get_plaintext_file_path() << std::endl;
+    string random_fp = get_randomized_file_path(filepath)
+    cout << "Randomized path: " << random_fp << std::endl;
+    cout << "PT_path: " << get_plaintext_file_path(random_fp) << std::endl;
     return 0;
 }
